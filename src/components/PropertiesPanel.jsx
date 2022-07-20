f28e65feeabc40b4bdfb425222e5e283a466f9f1
myketheguru <darkmyketheguru@gmail.com>
2022-07-20 09:04:57 +0100

    Added more styling capabilities to the property panel

--- src/components/PropertiesPanel.jsx
+++ src/components/PropertiesPanel.jsx
@@ -27,11 +27,30 @@ const PropertiesPanel = ({ rerender }) => {
     setDropzoneData(newData)
     rerender(Math.random() * 1000)
   }
+
+  const handlePaddingChange = (value, dir) => {
+    let newData = dropzoneData
+    newData[selectedDataIndex].styles = { 
+      ...newData[selectedDataIndex].styles, 
+      [dir === 'top' ? 'paddingTop' : dir === 'right' ? 'paddingRight' : dir === 'bottom' ? 'paddingBottom' : dir === 'left' ? 'paddingLeft' : 'padding']: value + 'px' }
+    setDropzoneData(newData)
+    rerender(Math.random() * 1000)
+  }
+  
+  const handleMarginChange = (value, dir) => {
+    let newData = dropzoneData
+    newData[selectedDataIndex].styles = { 
+      ...newData[selectedDataIndex].styles, 
+      [dir === 'top' ? 'marginTop' : dir === 'right' ? 'marginRight' : dir === 'bottom' ? 'marginBottom' : dir === 'left' ? 'marginLeft' : 'margin']: value + 'px' }
+    setDropzoneData(newData)
+    rerender(Math.random() * 1000)
+  }
   
 
   return selectedDataIndex >= 0 ? (
     <aside className="properties-panel" onClick={evt => evt.stopPropagation()}>
         <h3>Field Properties</h3>
+        <div className="inner">
         <div className="block">
             <p>Label Text</p>
             <textarea 
@@ -45,12 +64,133 @@ const PropertiesPanel = ({ rerender }) => {
                 cols="30" rows="5"></textarea>
         </div>
         <div className="block">
+            <p>Padding</p>
+            <input 
+              type="number"
+              placeholder='padding (All sides)'
+                // value={
+                //     dropzoneData[selectedDataIndex]?.styles.padding
+                // } 
+                name={'padding'}
+                onChange={evt => {
+                    handlePaddingChange(evt.target.value, 'all')
+                }} 
+               />
+               <div className="dir">
+                <input 
+                  type="number"
+                  placeholder='top'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.padding
+                    // } 
+                  name={'paddingTop'}
+                  onChange={evt => {
+                      handlePaddingChange(evt.target.value, 'top')
+                  }} 
+                />
+                <input 
+                  type="number"
+                  placeholder='right'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.padding
+                    // } 
+                  name={'paddingRight'}
+                  onChange={evt => {
+                      handlePaddingChange(evt.target.value, 'right')
+                  }} 
+                />
+                <input 
+                  type="number"
+                  placeholder='left'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.padding
+                    // } 
+                  name={'paddingLeft'}
+                  onChange={evt => {
+                      handlePaddingChange(evt.target.value, 'left')
+                  }} 
+                />
+                <input 
+                  type="number"
+                  placeholder='bottom'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.padding
+                    // } 
+                  name={'paddingBottom'}
+                  onChange={evt => {
+                      handlePaddingChange(evt.target.value, 'bottom')
+                  }} 
+                />
+               </div>
+        </div>
+        <div className="block">
+            <p>Margin</p>
+            <input 
+              type="number"
+              placeholder='margin (All sides)'
+                // value={
+                //     dropzoneData[selectedDataIndex]?.styles.margin
+                // } 
+                name={'margin'}
+                onChange={evt => {
+                    handleMarginChange(evt.target.value, 'all')
+                }} 
+               />
+               <div className="dir">
+                <input 
+                  type="number"
+                  placeholder='top'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.margin
+                    // } 
+                  name={'marginTop'}
+                  onChange={evt => {
+                      handleMarginChange(evt.target.value, 'top')
+                  }} 
+                />
+                <input 
+                  type="number"
+                  placeholder='right'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.margin
+                    // } 
+                  name={'marginRight'}
+                  onChange={evt => {
+                      handleMarginChange(evt.target.value, 'right')
+                  }} 
+                />
+                <input 
+                  type="number"
+                  placeholder='left'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.margin
+                    // } 
+                  name={'marginLeft'}
+                  onChange={evt => {
+                      handleMarginChange(evt.target.value, 'left')
+                  }} 
+                />
+                <input 
+                  type="number"
+                  placeholder='bottom'
+                    // value={
+                    //     dropzoneData[selectedDataIndex]?.styles.margin
+                    // } 
+                  name={'marginBottom'}
+                  onChange={evt => {
+                      handleMarginChange(evt.target.value, 'bottom')
+                  }} 
+                />
+               </div>
+        </div>
+        <div className="block">
             <p>Background Color</p>
             <ColorPicker onChange={({value, evt}) => handleBgColorChange(value)} defaultValue={dropzoneData[selectedDataIndex].styles.background} />
         </div>
         <div className="block">
             <p>Text Color</p>
             <ColorPicker onChange={({value, evt}) => handleColorChange(value)} defaultValue={dropzoneData[selectedDataIndex].styles.color} />
+        </div>
         </div>
     </aside>
   ) : (
